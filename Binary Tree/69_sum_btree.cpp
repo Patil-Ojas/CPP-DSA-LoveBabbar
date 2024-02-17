e1082190a434f12c856609cce2ecf882883f1c61
Ojas Patil <128805590+Patil-Ojas@users.noreply.github.com>
2024-02-17 19:15:34 +0530

    Add files via upload

---
+++ Binary Tree/69_sum_btree.cpp
@@ -0,0 +1,156 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+/*  Tree node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+// Should return true if tree is Sum Tree, else false
+class Solution
+{
+    public:
+    
+    pair <bool, int> isSum_fast(Node* root)
+    {
+        if (root==NULL)
+        {
+            pair <bool, int> p = make_pair(true, 0);
+            return p;
+        }
+        
+        if (root->left==NULL and root->right==NULL)
+        {
+            pair <bool, int> p = make_pair(true, root->data);
+            return p;
+        }
+        
+        pair <bool, int> left_sum = isSum_fast(root->left); 
+        pair <bool, int> right_sum = isSum_fast(root->right); 
+        
+        bool left_check = left_sum.first;
+        bool right_check = right_sum.first;
+        
+        bool sum_check = root->data == left_sum.second + right_sum.second;
+        
+        pair<bool, int> ans;
+        ans.second = 2*root->data;
+        
+        if (left_check and right_check and sum_check) 
+            ans.first = true;
+        else
+            ans.first = false;
+        
+        return ans;
+    }
+    
+    bool isSumTree(Node* root)
+    {
+         // Your code here
+         return isSum_fast(root).first;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+        Node* root = buildTree(s);
+        Solution ob;
+        cout <<ob.isSumTree(root) << endl;
+    }
+    return 1;
+}
+// } Driver Code Ends
