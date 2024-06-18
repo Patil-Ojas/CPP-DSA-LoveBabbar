7aee1cd7628a19943a290504d8c76fd1661e6a92
Ojas <ojas.abhijit.patil@gmail.com>
2024-06-18 10:42:21 +0530

    Updating graphs

--- Graph/99_graphq.cpp
+++ Graph/99_graphq.cpp
@@ -1,6 +1,6 @@
 
 vector < vector < int >> printAdjacency(int n, int m, vector < vector < int >> & edges) {
-    vector<int> ans(n);
+    vector<int> ans[n];
 
     for (int i=0; i<m; i++)
     {
@@ -11,13 +11,12 @@ vector < vector < int >> printAdjacency(int n, int m, vector < vector < int >> &
         ans[v].push_back(u);
     }
 
-    vector<vector<int>> adj;
-
+    vector<vector<int>> adj(n);
     for (int i=0; i<n; i++)
     {
         adj[i].push_back(i);
 
-        for (int j=0; j<n; j++)
+        for (int j=0; j<ans[i].size(); j++)
         {
             adj[i].push_back(ans[i][j]);
         }
