5b7a9fcce63586f3d6fe6f0b0f4e0be1d056f94e
Ojas <ojas.abhijit.patil@gmail.com>
2024-07-16 21:25:16 +0530

    start of the DP

--- DP/idk_start.cpp
+++ DP/idk_start.cpp
@@ -4,3 +4,33 @@
 //            -> bottom up
 
 // we use dp when we can get the optimized solution of a problem using the solution of a sub-section of the problem
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int fib( int n, vector<int> &dp)
+{
+        if (n<=1)
+                return n;
+        
+        if (dp[n]!=-1)
+                return dp[n];
+                
+        dp[n] = fib(n-1, dp) + fib(n-2, dp);
+        return dp[n];
+}
+
+int main()
+{
+        int n;
+        cin>>n;
+
+        vector<int> dp(n+1);
+        for (int i=0; i<=n; i++)
+        {
+                dp[i] = -1;
+        }
+
+        cout<<fib(n, dp);
+        return 0;
+}
