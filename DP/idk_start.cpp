ca8243857803b01f73f3ea6847137aeb01df9206
Ojas <ojas.abhijit.patil@gmail.com>
2024-07-16 19:53:59 +0530

    The beggining of DP

---
+++ DP/idk_start.cpp
@@ -0,0 +1,6 @@
+// those who forget the past, are condemned to repeat it
+
+// approaches -> top down
+//            -> bottom up
+
+// we use dp when we can get the optimized solution of a problem using the solution of a sub-section of the problem
